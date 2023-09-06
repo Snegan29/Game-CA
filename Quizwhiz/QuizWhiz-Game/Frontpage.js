253d0c9c14a82e22ab224164430d600238fa1803
Snegan P <114458196+Snegan29@users.noreply.github.com>
2023-09-06 11:10:47 +0530

    Add files via upload

---
+++ Quizwhiz/QuizWhiz-Game/Frontpage.js
@@ -0,0 +1,33 @@
+
+const popupButton = document.getElementById("popup-button");
+const popupContainer = document.getElementById("popup-container");
+const closePopup = document.getElementById("close-popup");
+
+
+popupButton.addEventListener("click", () => {
+    console.log(1)
+    popupContainer.style.display = "block";
+});
+
+
+closePopup.addEventListener("click", () => {
+    popupContainer.style.display = "none";
+});
+
+const showFormButton = document.getElementById("continue-btn");
+const myForm = document.getElementById("myForm");
+// const redirected = dociment.getElementById("submit")  
+
+function showpopup(){
+    myForm.style.display = "block";
+    showFormButton.style.display = "none"
+}
+
+document.getElementById('myForm').addEventListener('submit', function (e) {
+
+    e.preventDefault(); 
+
+    const inputData = document.getElementById('inputData').value;
+    const url = 'destination.html?userInput=' + encodeURIComponent(inputData);
+    window.location.href = "gameModule.html";
+});
